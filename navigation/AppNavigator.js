78bf434e1880e5930cebff33d4be07b97a3a8af6
angelgarcia09072004 <angelanne.garcia@neu.edu.ph>
2026-04-07 18:10:15 +0800

    Rename navigation file and fix App.js

--- navigation/AppNavigator.js
+++ navigation/AppNavigator.js
@@ -11,13 +11,12 @@ const Stack = createStackNavigator();
 function AppNavigator() {
   return (
     <NavigationContainer>
-      <Stack.Navigator 
+      <Stack.Navigator  
         initialRouteName="Login"
         screenOptions={{ headerShown: false }} // Hides the default top bar
       >
         {/* Screen 1: Login */}
         <Stack.Screen name="Login" component={LoginScreen} />
-        
         {/* Screen 2: Dashboard */}
         <Stack.Screen name="Dashboard" component={Dashboard} />
       </Stack.Navigator>
