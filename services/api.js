0e19b15a409441e18d1a31dc3ae930eb3f2c7c9a
angelgarcia09072004 <angelanne.garcia@neu.edu.ph>
2026-04-07 21:40:48 +0800

    commit api

--- services/api.js
+++ services/api.js
@@ -1,9 +1,11 @@
 import axios from 'axios';
 
 const API = axios.create({
-    baseURL: 'http://192.168.56.1:8000/api', 
+  baseURL: 'http://192.168.8.41:8000/api', 
+  headers: {
+    'Accept': 'application/json',
+    'Content-Type': 'application/json',
+  }
 });
-
-export default API;
 
 
