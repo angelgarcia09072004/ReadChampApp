7192a5fc104de37ddba59a1193ed3b4269268299
angelgarcia09072004 <angelanne.garcia@neu.edu.ph>
2026-04-07 18:14:45 +0800

    commit

--- navigation/AppNavigator.js
+++ navigation/AppNavigator.js
@@ -1,8 +1,3 @@
-import React from 'react';
-import { createStackNavigator } from '@react-navigation/stack';
-import { NavigationContainer } from '@react-navigation/native';
-
-// Import your screens
 import LoginScreen from '../screens/LoginScreen';
 import Dashboard from '../screens/Dashboard';
 
@@ -11,12 +6,13 @@ const Stack = createStackNavigator();
 function AppNavigator() {
   return (
     <NavigationContainer>
-      <Stack.Navigator  
+      <Stack.Navigator 
         initialRouteName="Login"
         screenOptions={{ headerShown: false }} // Hides the default top bar
       >
         {/* Screen 1: Login */}
         <Stack.Screen name="Login" component={LoginScreen} />
+        
         {/* Screen 2: Dashboard */}
         <Stack.Screen name="Dashboard" component={Dashboard} />
       </Stack.Navigator>
