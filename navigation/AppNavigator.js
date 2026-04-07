70825aa9457227ebaae63cb892509ea31700f5d8
angelgarcia09072004 <angelanne.garcia@neu.edu.ph>
2026-04-07 19:02:30 +0800

    commit

--- navigation/AppNavigator.js
+++ navigation/AppNavigator.js
@@ -1,20 +1,23 @@
 import React from 'react';
 import WelcomeScreen from '../screens/WelcomeScreen';
-import { NavigationContainer } from '@react-navigation/native'; 
-import { createStackNavigator } from '@react-navigation/stack'; 
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
 import LoginScreen from '../screens/LoginScreen';
 import Dashboard from '../screens/Dashboard';
+import RoleSelectionScreen from '../screens/RoleSelectionScreen'; // <--- ADD THIS LINE!
 
 const Stack = createStackNavigator();
 
 function AppNavigator() {
   return (
     <NavigationContainer>
-      <Stack.Screen name="Welcome" component={WelcomeScreen} />
-      <Stack.Screen name="RoleSelection" component={RoleSelectionScreen} />
-      <Stack.Screen name="Login" component={LoginScreen} />
-      <Stack.Screen name="Dashboard" component={Dashboard} />
+      <Stack.Navigator initialRouteName="Welcome" screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="Welcome" component={WelcomeScreen} />
+        <Stack.Screen name="RoleSelection" component={RoleSelectionScreen} />
+        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="Dashboard" component={Dashboard} />
+      </Stack.Navigator>
     </NavigationContainer>
   );
 }
